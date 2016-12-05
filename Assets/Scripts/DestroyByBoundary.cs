de352685cbf1197c5878960cac0e251b93000594
kubmari <kubmari@gmail.com>
2016-12-05 15:43:42 +0100

    First publish of Official Unity Space Shooter Tutorial

---
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByBoundary : MonoBehaviour {
+    private void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
---
+++ Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour {
+
+    public float lifetime;
+
+    private void Start()
+    {
+        Destroy(gameObject,lifetime);
+    }
+}
